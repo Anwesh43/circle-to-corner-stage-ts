1b59602d250d403aa4ccebfadbc07cc8b405049b
Anwesh43 <anweshthecool0@gmail.com>
2018-11-11 12:16:58 +0530

    adding CircleToCornerSquareStage class

--- CircleToCornerSquareStage.ts
+++ CircleToCornerSquareStage.ts
@@ -77,3 +77,34 @@ const drawCTCSNode : Function = (context : CanvasRenderingContext2D, i : number,
     }
     context.restore()
 }
+
+class CircleToCornerSquareStage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = '#212121'
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : CircleToCornerSquareStage = new CircleToCornerSquareStage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
