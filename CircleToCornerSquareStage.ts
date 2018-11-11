44f772fdbdb236453ac9081cf147803b75245e82
Anwesh43 <anweshthecool0@gmail.com>
2018-11-11 11:43:34 +0530

    adding number methods

--- CircleToCornerSquareStage.ts
+++ CircleToCornerSquareStage.ts
@@ -7,3 +7,19 @@ const STROKE_FACTOR : number = 60
 const lines : number = 4
 const circles : number = 4
 const nodes : number = 4
+const parts : number = 2
+
+const getInverse : Function = (n : number) : number => 1 / n
+
+const divideScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.min(getInverse(n), Math.max(0, scale - i * getInverse(n))) * n
+}
+
+const getScaleFactor : Function = (scale : number) : number => {
+    return Math.floor(scale * getInverse(parts))
+}
+
+const updateScale : Function = (scale : number, dir : number, a : number, b : number) : number => {
+    const sf = getScaleFactor(scale)
+    return dir * (0.1 * getInverse(parts)) * ((1 - sf) * getInverse(a) + sf * getInverse(b))
+}
