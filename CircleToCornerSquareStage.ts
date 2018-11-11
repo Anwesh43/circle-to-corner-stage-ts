0fb3067fc0bac0cef4e5295ac5f74c288ec6835d
Anwesh43 <anweshthecool0@gmail.com>
2018-11-11 12:28:32 +0530

    adding CTCSNode class

--- CircleToCornerSquareStage.ts
+++ CircleToCornerSquareStage.ts
@@ -150,3 +150,47 @@ class Animator {
         }
     }
 }
+
+class CTCSNode {
+    prev : CTCSNode
+    next : CTCSNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new CTCSNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        drawCTCSNode(context)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : CTCSNode {
+        var curr : CTCSNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
