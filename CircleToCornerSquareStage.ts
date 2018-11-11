be59603c86291f773dde5faca014a047a9e91165
Anwesh43 <anweshthecool0@gmail.com>
2018-11-11 11:27:32 +0530

    adding constants

---
+++ CircleToCornerSquareStage.ts
@@ -0,0 +1,9 @@
+const w : number = window.innerWidth, h : number = window.innerHeight
+const DELAY : number = 50
+const color : String = "#01579B"
+const SIZE_FACTOR : number = 3
+const RADIUS_FACTOR : number = 4
+const STROKE_FACTOR : number = 60
+const lines : number = 4
+const circles : number = 4
+const nodes : number = 4
